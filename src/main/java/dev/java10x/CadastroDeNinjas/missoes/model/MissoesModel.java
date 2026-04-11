1c173af33299b121d20ec46cd17800568513841d
Daniel <danieldfcreis@gmail.com>
2026-04-11 17:52:44 -0300

    feat: coloca o lombok na classe

--- src/main/java/dev/java10x/CadastroDeNinjas/missoes/model/MissoesModel.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/missoes/model/MissoesModel.java
@@ -2,11 +2,17 @@ package dev.java10x.CadastroDeNinjas.missoes.model;
 
 import dev.java10x.CadastroDeNinjas.ninjas.model.NinjaModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Entity
 @Table(name = "tb_missoes")
+@NoArgsConstructor // construtor sem argumentos
+@AllArgsConstructor // construtor com todos argumentos
+@Data //gera os getters e setters
 public class MissoesModel {
 
     @Id
@@ -21,47 +27,4 @@ public class MissoesModel {
     @OneToMany(mappedBy = "missoes") //mapeando para onde enviar os Ninjas
     private List<NinjaModel> ninja;
 
-    public MissoesModel() {
-    }
-
-    public MissoesModel(Long id, String dificuldade, String name) {
-        this.id = id;
-        this.dificuldade = dificuldade;
-        this.name = name;
-        this.ninja = ninja;
-    }
-
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getDificuldade() {
-        return dificuldade;
-    }
-
-    public void setDificuldade(String dificuldade) {
-        this.dificuldade = dificuldade;
-    }
-
-    @Override
-    public String toString() {
-        return "MissoesModel{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", dificuldade='" + dificuldade + '\'' +
-                '}';
-    }
 }
--- src/main/java/dev/java10x/CadastroDeNinjas/ninjas/model/NinjaModel.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/ninjas/model/NinjaModel.java
@@ -2,12 +2,18 @@ package dev.java10x.CadastroDeNinjas.ninjas.model;
 
 import dev.java10x.CadastroDeNinjas.missoes.model.MissoesModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 //Trasforma classe em entidade no banco de dados
 @Entity
 @Table(name = "tb_cadastro")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class NinjaModel {
 
     @Id
@@ -24,41 +30,5 @@ public class NinjaModel {
     @ManyToOne
     @JoinColumn(name = "missoes_id")//chave estrangeira
     private MissoesModel missoes;
-
-
-    public NinjaModel(String nome, String email, Integer idade) {
-        this.nome = nome;
-        this.email = email;
-        this.idade = idade;
-    }
-
-    public NinjaModel() {
-    }
-
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public Integer getIdade() {
-        return idade;
-    }
-
-    public void setIdade(Integer idade) {
-        this.idade = idade;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
 
 }
