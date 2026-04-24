2f6688e1ca940563c6a0a2c63d49fdff60345763
Daniel <danieldfcreis@gmail.com>
2026-04-23 22:39:10 -0300

    refactor: adiciona novas funcoes ao codigo e adicona missoesDto

---
+++ src/main/java/dev/java10x/CadastroDeNinjas/missoes/dto/MissaoDTO.java
@@ -0,0 +1,4 @@
+package dev.java10x.CadastroDeNinjas.missoes.dto;
+
+public class MissaDTO {
+}
---
+++ src/main/java/dev/java10x/CadastroDeNinjas/missoes/mapper/MissaoMapper.java
@@ -0,0 +1,4 @@
+package dev.java10x.CadastroDeNinjas.missoes.mapper;
+
+public class MissaoMapper {
+}
